00dbc2c47a8b1f118f646dbefb7f07c1e37c0a3c
Umran Jameel <umranrazack02@mak-bwk-lmran.local>
2021-06-05 22:40:26 -0400

    Initial commit

---
+++ src/main/java/Exercise4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = input.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = input.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = input.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adverb = input.nextLine();
+
+        String str = String.format("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+        System.out.println(str);
+
+    }
+}
